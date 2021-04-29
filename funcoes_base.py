b92e9bcb2d94f26fcb376905dce20619a0f68f21
Guidolscr <81170424+Guidolscr@users.noreply.github.com>
2021-04-29 19:17:08 -0300

    Update funcoes_base.py

--- funcoes_base.py
+++ funcoes_base.py
@@ -10,4 +10,15 @@ def extrai_valor(carta):
     return carta[0:-1]
 def extrai_naipe(carta):
     return carta[-1]
-
+def lista_movimentos_possiveis(baralho,indice):
+    movs = []
+    carta = baralho[indice]
+    if indice-1 >=0:
+        anterior = baralho[indice-1]
+        if extrai_naipe(carta) == extrai_naipe(anterior) or extrai_valor(carta) == extrai_valor(anterior):
+            movs.append(1)
+    if indice-3 >=0:
+        ter_anterior = baralho[indice-3]
+        if extrai_naipe(carta) == extrai_naipe(ter_anterior) or extrai_valor(carta) == extrai_valor(ter_anterior):
+            movs.append(3)
+    return movs

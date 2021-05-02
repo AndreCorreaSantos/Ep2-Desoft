6d17cf40804fee60795e282764192b07abfd2a7a
André Corrêa Santos <54818184+AndreCorreaSantos@users.noreply.github.com>
2021-05-02 15:31:00 -0300

    Update funcoes_base.py

--- funcoes_base.py
+++ funcoes_base.py
@@ -31,3 +31,24 @@ def possui_movimentos_possiveis(baralho):
         if len(lista_movimentos_possiveis(baralho,indice)) > 0:
             return True
     return False
+
+def programa():
+    inicio = "a"
+    baralho = cria_baralho()
+    random.shuffle(baralho)
+    while inicio != "":
+        inicio = input("Paciência Acordeão \n seja bem vindo(a) ao jogo de Paciência Acordeão! O objetivo deste jogo é  colocar todas as cartas em uma mesma pilha. \n Existem apenas dois movimentos possíveis \n 1. Empilhar uma carta sobre a carta imediatamente anterior \n 2. Empilhar uma carta sobre a terceira carta anterior. \n Para que um movimento possa ser realizado basta que uma das duas condições abaixo seja atendida: \n 1. As duas cartas possuem o mesmo valor ou \n 2. As duas cartas possuem o mesmo naipe. \n Desde que alguma das condições acima seja satisfeita, qualquer carta pode ser movimentada. \n Aperte [ENTER] para iniciar o jogo...")
+    while True:
+        jogada = input(situacao(baralho))
+        if pode_int(jogada):
+            jogada = int(jogada)
+            jogada_index = jogada-1
+            if 1 <= jogada <= len(baralho) and len(lista_movimentos_possiveis(baralho,jogada_index)):
+                jogada_index = jogada-1
+                movimento = lista_movimentos_possiveis(baralho,jogada_index)[0]
+                baralho = empilha(baralho,jogada_index,jogada_index-movimento)
+                if not possui_movimentos_possiveis(baralho) and len(baralho) == 1:
+                    print("Parabéns você ganhou")
+                    programa()
+                if not possui_movimentos_possiveis(baralho) and len(baralho) > 1 :
+                    print("Você perdeu")

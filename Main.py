3da320bd3f5d89b62a7e8cb914ca1cc9389038f0
André Corrêa Santos <54818184+AndreCorreaSantos@users.noreply.github.com>
2021-05-02 23:57:24 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,14 @@
-import funcoes_base.py
+import funcoes_base as funcs
+import random
+
+def programa():
+    inicio = "a"
+    baralho = funcs.cria_baralho()
+    random.shuffle(baralho)
+    while inicio != "":
+        inicio = input(regras)
+    funcs.intermediario(baralho)
+
+programa()
 
 

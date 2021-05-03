90387b8062db138991c2cf8a0b2dd280b207d34f
André Corrêa Santos <54818184+AndreCorreaSantos@users.noreply.github.com>
2021-05-03 00:07:52 -0300

    Update Main.py
    corrigindo erros de chamada do modulo de funcoes

--- Main.py
+++ Main.py
@@ -1,14 +1,12 @@
-import funcoes_base as funcs
+from funcoes_base 
 import random
 
 def programa():
     inicio = "a"
-    baralho = funcs.cria_baralho()
+    baralho = cria_baralho()
     random.shuffle(baralho)
     while inicio != "":
         inicio = input(regras)
-    funcs.intermediario(baralho)
+    intermediario(baralho)
 
 programa()
-
-

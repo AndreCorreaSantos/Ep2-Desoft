873ba06fe2aeeb37d977e49ce66d7fdb7ff73b6c
André Corrêa Santos <54818184+AndreCorreaSantos@users.noreply.github.com>
2021-05-02 22:46:20 -0300

    Update funcoes_base.py

--- funcoes_base.py
+++ funcoes_base.py
@@ -51,26 +51,31 @@ def situacao(baralho):
     string = "O Estado atual do baralho é: \n {} \n Escolha uma carta (digite um número entre 1 e {}):   ".format(string,len(baralho))
     return string
     
+ #funcao intermediaria que chama a si mesma, substituindo a necessidade de um loop--> funcao programa chama a intermediaria
+def intermediario():
+while True:
+    jogada = input(situacao(baralho))
+    if pode_int(jogada):
+        jogada = int(jogada)
+        jogada_index = jogada-1
+    if 1 <= jogada <= len(baralho) and len(lista_movimentos_possiveis(baralho,jogada_index)):
+        jogada_index = jogada-1
+        movimento = lista_movimentos_possiveis(baralho,jogada_index)[0]
+        baralho = empilha(baralho,jogada_index,jogada_index-movimento)
+        if not possui_movimentos_possiveis(baralho) and len(baralho) == 1:
+            print("Parabéns você ganhou")
+            programa()
+        if not possui_movimentos_possiveis(baralho) and len(baralho) > 1 :
+            print("Você perdeu")
+            programa()
+        else:
+            programa()
+                    
+
 def programa():
     inicio = "a"
     baralho = cria_baralho()
     random.shuffle(baralho)
     while inicio != "":
         inicio = input(regras)
-    while True:
-        jogada = input(situacao(baralho))
-        if pode_int(jogada):
-            jogada = int(jogada)
-            jogada_index = jogada-1
-            if 1 <= jogada <= len(baralho) and len(lista_movimentos_possiveis(baralho,jogada_index)):
-                jogada_index = jogada-1
-                movimento = lista_movimentos_possiveis(baralho,jogada_index)[0]
-                baralho = empilha(baralho,jogada_index,jogada_index-movimento)
-                if not possui_movimentos_possiveis(baralho) and len(baralho) == 1:
-                    print("Parabéns você ganhou")
-                    programa()
-                if not possui_movimentos_possiveis(baralho) and len(baralho) > 1 :
-                    print("Você perdeu")
-                    programa()
-                else:
-                    programa()
+            intermediario()
